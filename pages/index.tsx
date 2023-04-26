8d7f8b2cfdb12cb20f34382807c40c068491a1b0
Luis Gurmendez <luisgurmendez@gmail.com>
2023-04-26 13:21:06 +0200

    added shit

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,118 +1,248 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import FlexSearch from 'flexsearch';
+import Fuse from 'fuse.js';
+import { fuzzyFilter } from 'fuzzbunny';
+import { matchSorter } from 'match-sorter';
+import { ChangeEvent, useCallback, useMemo, useState } from 'react';
 
-const inter = Inter({ subsets: ['latin'] })
+interface Ammenity{
+  name: string;
+}
+
+interface SearchResults{
+  fuse: Ammenity[];
+  flexsearch: Ammenity[];
+  matchSorter: Ammenity[];
+  fuzzbunny: Ammenity[];
+}
+
 
 export default function Home() {
+  const ammenities = useAmmenities();
+  const [query, setQuery] = useState('');
+  const search = useSearch(ammenities);
+  
+  const [results, setResults] = useState<SearchResults>({
+    fuse: [],
+    flexsearch: [],
+    matchSorter: [],
+    fuzzbunny: [],
+  });
+
+  const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const q = e.target.value;
+    setQuery(q)
+    setResults(search(q));
+  }
+
+
   return (
     <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
+      className={`flex min-h-screen flex-col items-center p-24`}
     >
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">pages/index.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+      <span className="text-4xl mb-8 font-medium ">Buscador de hoteles Madrid</span>
+      <input className="p-2 rounded-md" value={query} placeholder="Busca por nombre" onChange={handleInputChange}/>
+
+      <div className='flex'>
+        <SearchResult results={results.fuse} name="fuse"  />
+        <SearchResult results={results.flexsearch} name="flexsearch" />
+        <SearchResult results={results.matchSorter} name="matchSorter" />
+        <SearchResult results={results.fuzzbunny} name="fuzzbunny" />
       </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+      <span className="text-lg mt-8 font-medium">Lista completa</span>
+      <SearchResult results={ammenities} name="" />
     </main>
   )
+}
+
+
+function useSearch(ammenities: Ammenity[]): (query: string) => SearchResults{
+  const fuseSearch = useFuse(ammenities);
+  const flexsearchSearch = useFlexsearch(ammenities)
+  const matchSorterSearch = useMatchSorter(ammenities)
+  const fuzzbunnySearch = useFuzzbunny(ammenities)
+
+return useCallback((query: string)=>{
+  const fuseResults = fuseSearch(query)
+  const flexsearchResults = flexsearchSearch(query)
+  const matchSorterResults = matchSorterSearch(query)
+  const fuzzbunnyResults = fuzzbunnySearch(query)
+
+  const results: SearchResults = {
+      fuse: fuseResults,
+      flexsearch: flexsearchResults,
+      matchSorter: matchSorterResults,
+      fuzzbunny: fuzzbunnyResults
+    }
+
+  return results;
+  }, [
+      fuseSearch,
+      flexsearchSearch,
+      matchSorterSearch,
+      fuzzbunnySearch,
+    ]
+  );
+}
+
+
+function useFuse(ammenities: Ammenity[]){
+  const fuse = useMemo(() => new Fuse(ammenities, {includeScore: true, keys: ["name"]}), [ammenities])
+  return useCallback((query:string) => {
+      if(query === '') return [];
+      return fuse.search(query).map(r => r.item);
+  }, [fuse]);
+}
+
+
+function useFlexsearch(ammenities: Ammenity[]){
+  return useCallback((query:string) => {
+    // const index = FlexSearch.create();
+    // ammenities.forEach((h, i) => {
+    //     index.add(i,h.name);
+    // })
+    return [];
+    // return index.search(query);
+  }, [ammenities]);
+}
+
+function useMatchSorter(ammenities: Ammenity[]){
+  return useCallback((query:string) => {
+    if(query === '') return [];
+    return matchSorter(ammenities, query, {keys: ['name']})
+  }, [ammenities]);
+}
+
+
+function useFuzzbunny(ammenities: Ammenity[]){
+  return useCallback((query:string) => {
+      if(query === '') return [];
+      return fuzzyFilter(ammenities, query, {fields: [`name`]}).map(a => a.item);
+  }, [ammenities]);
+}
+
+function useAmmenities(): Ammenity[]{
+  return useMemo(()=>{
+    const hotels = [
+      "Hotel Riu Plaza España",
+      "Hard Rock Hotel Madrid",
+      "Hotel Maydrit Airport",
+      "Senator Barajas",
+      "Madrid Marriott Auditorium Hotel &amp; Conference Center",
+      "Melia Barajas",
+      "Hotel Regina",
+      "Art Seven Hostel Capsules",
+      "Hotel Nuevo Madrid",
+      "Hotel Nuevo Boston",
+      "Hotel Atlantico Madrid",
+      "Hilton Madrid Airport",
+      "Hotel Mediodia",
+      "Pestana CR7 Gran Vía Madrid",
+      "Hotel Europa",
+      "Eurostars Madrid Tower",
+      "Hotel Intur Palacio San Martin",
+      "Room Mate Macarena - Gran Vía",
+      "Hotel Princesa Plaza Madrid",
+      "Hotel Madrid Alameda Aeropuerto, Affiliated by Meliá",
+      "VP Plaza España Design",
+      "Barceló Emperatriz",
+      "Axor Barajas",
+      "Hotel Madrid Gran Via 25 Affiliated by Meliá",
+      "Barceló Torre de Madrid",
+      "Barceló Imagine",
+      "Radisson RED Madrid",
+      "NH Collection Palacio de Tepa",
+      "Hotel Emperador",
+      "SLEEP’N Atocha – B Corp Certified",
+      "NH Madrid Nacional",
+      "Hotel Principe Pio",
+      "Petit Palace Savoy Alfonso XII",
+      "NH Madrid Barajas Airport",
+      "ICON Embassy",
+      "ibis budget Madrid Centro Lavapies",
+      "Only YOU Boutique Hotel",
+      "Hotel Porcel Ganivet",
+      "Senator Castellana Hotel",
+      "The Westin Palace, Madrid",
+      "Hyatt Centric Gran Via Madrid",
+      "NH Madrid Lagasca",
+      "NH Villa de Coslada",
+      "Axor Feria",
+      "Pestana Plaza Mayor Madrid",
+      "Hotel Santo Domingo",
+      "room007 Select Sol",
+      "NH Collection Madrid Suecia",
+      "Hotel Liabeny",
+      "Novotel Madrid Center",
+      "H10 Puerta de Alcalá",
+      "Hotel ILUNION Pio XII",
+      "Rosewood Villa Magna",
+      "NH Collection Madrid Gran Vía",
+      "NH Collection Madrid Colón",
+      "Hotel ILUNION Suites Madrid",
+      "Agumar Hotel",
+      "Hotel Claridge Madrid",
+      "Hotel ibis Madrid Aeropuerto Barajas",
+      "Axel Hotel Madrid - Adults Only",
+      "NH Madrid Príncipe de Vergara",
+      "Hostal Abadía Madrid",
+      "Hotel Fenix Gran Meliá - The Leading Hotels of the World ",
+      "Melia Avenida América",
+      "NH Collection Paseo del Prado",
+      "Catalonia Gran Via Madrid",
+      "NH Collection Madrid Eurobuilding",
+      "Urban",
+      "Canopy by Hilton Madrid Castellana",
+      "Room Mate Óscar",
+      "Wellington Hotel &amp; Spa Madrid",
+      "Far Home Bernabeu",
+      "Only YOU Hotel Atocha",
+      "Generator Madrid",
+      "Le Petit Palü-Fuencarral",
+      "ME Madrid by Melia",
+      "Hotel Preciados",
+      "TOC Hostel and Suites Madrid",
+      "Hotel Clement Barajas",
+      "C&amp;H Aravaca Garden",
+      "Petit Palace President Castellana",
+      "Hotel Exe Plaza",
+      "The Principal Madrid",
+      "Four Seasons Hotel Madrid",
+      "Hotel ILUNION Alcalá Norte",
+      "Casual del Teatro Madrid",
+      "Relais &amp; Châteaux Heritage Madrid",
+      "Air Rooms Madrid by Premium Traveller",
+      "Hotel ILUNION Atrium",
+      "Hotel Villa De Barajas"
+  ]
+
+  return hotels.map(h => ({name: h}))
+  }, []);
+}
+
+
+
+const SearchResult = ({results, name}: {results: Ammenity[], name: string}) => {
+
+  return (
+    <div className="border-1 rounded-sm shadow-sm m-4 bg-white p-8">
+        <span className="text-lg font-medium ">{name}</span>
+        <table className="text-sm font-light">
+          <thead className="border-b font-medium dark:border-neutral-500">
+            <tr>
+              <th scope="col" className="px-2 py-2">#</th>
+              <th scope="col" className="px-2 py-2">Nombre</th>
+            </tr>
+          </thead>
+          <tbody>
+            {results.map((r,i) => (<tr key={i} className="border-b dark:border-neutral-500">
+              <td className="whitespace-nowrap px-2 py-2 font-medium">{i}</td>
+              <td className="px-2 py-2">{r.name}</td>
+            </tr>))}
+          </tbody>
+        </table>
+    </div>
+
+  )
+
 }
